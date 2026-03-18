c6f46c3e1e8edc3273864e26923b974e77a1a951
Swirl86 <swesuzzi@gmail.com>
2026-03-18 14:03:40 +0100

    Initial Spring Boot project setup

---
+++ ecom-engine/src/main/java/com/swirl/ecom_engine/EcomEngineApplication.java
@@ -0,0 +1,13 @@
+package com.swirl.ecom_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomEngineApplication.class, args);
+	}
+
+}
---
+++ ecom-engine/src/test/java/com/swirl/ecom_engine/EcomEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swirl.ecom_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
